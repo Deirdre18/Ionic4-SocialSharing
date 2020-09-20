b9aec5ae7b74ebcf086f0c49121f9e15bacc9771
Deirdre18 <deirdreweldon18@gmail.com>
2020-09-21 00:58:07 +0100

    changes to package.json, config.xml, gradle-wrapper, config.xml, index.html, app.module.ts, home.page.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 import { File } from '@ionic-native/file/ngx';
- 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
- 
+
   text = 'Check out the Ionic Academy!';
   url = 'https://ionicacademy.com';
- 
+
   constructor(private socialSharing: SocialSharing, private file: File) { }
- 
+
   async shareTwitter() {
     // Either URL or Image
     this.socialSharing.shareViaTwitter(null, null, this.url).then(() => {
@@ -22,7 +22,7 @@ export class HomePage {
       // Error!
     });
   }
- 
+
   async shareWhatsApp() {
     // Text + Image or URL works
     this.socialSharing.shareViaWhatsApp(this.text, null, this.url).then(() => {
@@ -31,28 +31,28 @@ export class HomePage {
       // Error!
     });
   }
- 
+
   async resolveLocalFile() {
     return this.file.copyFile(`${this.file.applicationDirectory}www/assets/imgs/`, 'academy.jpg', this.file.cacheDirectory, `${new Date().getTime()}.jpg`);
   }
- 
+
   removeTempFile(name) {
     this.file.removeFile(this.file.cacheDirectory, name);
   }
- 
+
   async shareEmail() {
     let file = await this.resolveLocalFile();
- 
+
     this.socialSharing.shareViaEmail(this.text, 'My custom subject', ['saimon@devdactic.com'], null, null, file.nativeURL).then(() => {
       this.removeTempFile(file.name);
     }).catch((e) => {
       // Error!
     });
   }
- 
+
   async shareFacebook() {
     let file = await this.resolveLocalFile();
- 
+
     // Image or URL works
     this.socialSharing.shareViaFacebook(null, file.nativeURL, null).then(() => {
       this.removeTempFile(file.name);
